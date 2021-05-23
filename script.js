3778f8550975e4d19dd9f2663c6d65581beac686
Tierra <50035689+RuckerTierra@users.noreply.github.com>
2021-05-23 19:16:16 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let countEl = document.getElementById("count-el")
+
+// change the count-el in the HTML to reflect the new count
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    console.log(count)
+}
+
+// save(){
+//   console.log();
+// }
